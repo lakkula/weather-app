10e9c3c0a3e4d8b11f79ce0cf3cd18129c96d8a2
Kulani Lakshika <lakkula@gmail.com>
2024-01-20 20:34:53 +0530

    feat: add error massage to invalid city

--- script.js
+++ script.js
@@ -7,9 +7,14 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    if(response.status == 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }else{
     var data = await response.json();
 
-    console.log(data);
+    //console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
@@ -33,6 +38,10 @@ async function checkWeather(city){
     else if (data.weather[0].main == "Snow"){
         weatherIcon.src = "images/snow.png";
     }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+}
 }
 
 searchBtn.addEventListener("click", ()=>{
