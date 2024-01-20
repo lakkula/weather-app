f0a2605c4ade7d96c7f0370a4b80678e61f83bd7
Kulani Lakshika <lakkula@gmail.com>
2024-01-20 19:59:11 +0530

    feat: add weather API

---
+++ script.js
@@ -0,0 +1,22 @@
+const apiKey = "e2f7ff5fbb3d01a5fa26ee45ec4a34b9";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button"); 
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+}
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
+
